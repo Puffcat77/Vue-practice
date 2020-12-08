9aa5cccf8e40cf7b895e995a2cbb5bde6ca10c4a
Акопян Александр Айрапетович <akopia-sasha@mail.ru>
2020-12-08 15:36:31 +0300

    add base vue app with interface

---
+++ vue-pannellum-interface/src/components/PanoramaSelector.vue
@@ -0,0 +1,29 @@
+<template>
+    <select
+        @change='sceneChanged()'
+        v-model='selected'>
+        <option disabled value=''>{{baseOptionText}}</option>
+        <option v-for='scene in scenes'
+            :key='scene.id'
+            :value='scene'>{{ scene.title }}</option>
+    </select>
+</template>
+
+<script>
+export default {
+    props: [
+        'scenes',
+        'baseOptionText'
+    ],
+    data() {
+        return {
+            selected: ''
+        }
+    },
+    methods:{
+        sceneChanged() {
+            this.$emit('sceneChanged', this.selected)
+        }
+    }
+}
+</script>
